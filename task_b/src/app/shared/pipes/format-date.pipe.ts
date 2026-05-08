ab8632c513ed4fd891d77286fa375e2c65a8f5fb
HG-0-1 <alfayezh844@gmail.com>
2026-05-08 19:26:13 +0300

    the first sum

---
+++ task_b/src/app/shared/pipes/format-date.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+    name: 'formatDate',
+    standalone: true
+})
+export class FormatDatePipe implements PipeTransform{
+transform(value: string | number | Date): string {
+    if (!value) return '';
+    const date = new Date(value);
+
+    if (isNaN(date.getTime())){
+        return 'Invalid date';
+    }
+    return date.toLocaleString();
+}
+}
